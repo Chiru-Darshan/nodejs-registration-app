6178808ce7e1f4dc5b6a222111461a21a3a1fff5
eq04978 <darshan.a@microfocus.com>
2021-11-24 21:14:43 +0530

    added validators to form inputs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const routes = require('./routes/index')
 const express = require('express')
+const bodyParser = require('body-parser')
 const path = require('path')
 
 const app = express()
@@ -8,7 +9,10 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.use('/', routes);
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 const express = require('express')
+const {
+    body,
+    validationResult
+} = require('express-validator/check')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('form')
+    console.log(req.body)
+    res.render('form', {
+        title: "Registration Form"
+    })
+})
+router.post('/', [
+    body('name')
+    .isLength({
+        min: 1
+    })
+    .withMessage('Please enter a name'),
+    body('email')
+    .isLength({
+        min: 1
+    })
+    .withMessage('Please enter email')
+
+], (req, res) => {
+    const errors = validationResult(req)
+
+    if (errors.isEmpty()) {
+        res.send(' Thank you for registration!')
+    } else {
+
+    }
+    console.log(req.body)
+    res.render('form', {
+        title: "Registration Form",
+        errors: errors.array(),
+        data: req.body
+    })
 })
 
 module.exports = router;
